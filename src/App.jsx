6852e1a0fe8d7d81150a0b176e7d7e96fb525a56
sujonbiswas01 <sujonbiswasdev@gmail.com>
2025-07-25 06:29:21 +0600

    hero ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import {Button, HeroUIProvider} from "@heroui/react";
+import Button1 from './components/Button';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className='text-center'>hello word</h1>
+    <HeroUIProvider>
+ <h1 className='text-center'>hello word</h1>
+ <Button1/>
+    </HeroUIProvider>
+   
       
     </>
   )
---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+import {Button} from "@heroui/react";
+
+export default function Button1() {
+  return <Button color="primary">sujon</Button>;
+}

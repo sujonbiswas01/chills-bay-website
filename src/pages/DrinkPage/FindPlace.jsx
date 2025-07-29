2754986471a96923367ea294f6b0b1ba4f640bd6
sujonbiswas01 <sujonbiswasdev@gmail.com>
2025-07-30 03:01:01 +0600

    drink section done

--- src/pages/DrinkPage/FindPlace.jsx
+++ src/pages/DrinkPage/FindPlace.jsx
@@ -4,7 +4,7 @@ import {DatePicker} from "@heroui/react";
 import {TimeInput} from "@heroui/react";
 const FindPlace = () => {
   return (
-    <section className='container mx-auto overflow-x-hidden'>
+    <section className='container mx-auto overflow-x-hidden -mt-60 md:mt-4' >
         <h2 className='text-center'>Find the best places to eat and drink for any occasion</h2>
         <div className='bg-[#FBFCFF] w-full lg:w-[85%] gap-10 h-auto lg:h-[150px] py-10 px-10 lg:ms-10 rounded-xl'>
                     <div className='grid lg:grid-cols-6 grid-cols-1 gap-2'>
